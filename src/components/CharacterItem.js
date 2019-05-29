753c874649958b8b18eb5e7fd08bbcb4803e26fc
Carlos Us Santiago <cdus12@gmail.com>
2019-05-28 17:41:15 -0700

    updated character item. added this.prop

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -2,11 +2,12 @@ import Component from './Component.js';
 
 class CharacterItem extends Component {
     renderTemplate() {
+        const character = this.props.character;
         return /*html*/ `
             <li>
-                <h2>46th Earth King</h2>
-                <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
-                <p>Affiliation: Earth Kingdom Earth Kingdom Royal Family</p>
+                <h2>${character.name}</h2>
+                <img src="${character.image}" alt="${character.name}">
+                <p>${character.affiliation}</p>
             </li>
         `;
     }
