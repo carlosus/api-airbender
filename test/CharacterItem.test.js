b8b59b9ced5ffba4eba4d8ade1965c4359885c3b
Carlos Us Santiago <cdus12@gmail.com>
2019-05-29 22:55:49 -0700

    updated character item test

--- test/CharacterItem.test.js
+++ test/CharacterItem.test.js
@@ -17,6 +17,10 @@ test('make item from data', assert => {
         <li>
             <h2>46th Earth King</h2>
             <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
+            <div>
+                <a href="#?enemies=46th+Earth+King">Enemies</a>
+                <a href="#?allies=46th+Earth+King">Allies</a>
+            </div>
         </li>
     `);
 }); 
